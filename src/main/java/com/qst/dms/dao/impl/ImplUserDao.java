f5210ac4dabff05dd9c7cdba9638b836922369c1
俭锋 梁 <2402901363@qq.com>
2020-10-23 09:59:37 +0800

    添加了一些注释以及删除以一些无用代码

--- src/main/java/com/qst/dms/dao/impl/ImplUserDao.java
+++ src/main/java/com/qst/dms/dao/impl/ImplUserDao.java
@@ -1,6 +1,5 @@
 package com.qst.dms.dao.impl;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/com/qst/dms/util/VerificationUtil.java
+++ src/main/java/com/qst/dms/util/VerificationUtil.java
@@ -11,8 +11,8 @@ import java.util.regex.Pattern;
 public class VerificationUtil {
   /**
    * 验证传入的content是否符合regex的格式要求
- * @param content
- * @param regex
+ * @param content 需要被判定的内容
+ * @param regex 判断的正则表达式
  * @return 满足正则表达式则返回true，否则返回false
  */
 public static boolean isLegalFormat(String content,String regex){
